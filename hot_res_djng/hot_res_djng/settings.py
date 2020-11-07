4db04fb30e6d422dbfc30a4ec56d69a511199940
november-pain <max.chikita@gmail.com>
2020-11-07 19:50:58 +0200

    app configuration

--- hot_res_djng/hot_res_djng/settings.py
+++ hot_res_djng/hot_res_djng/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hot_res_app.apps.HotResAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
