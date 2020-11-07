aa4a0945191640bb0329c27a64274cc1ae0885fe
november-pain <max.chikita@gmail.com>
2020-11-07 19:50:23 +0200

    added home view

--- hot_res_djng/hot_res_app/views.py
+++ hot_res_djng/hot_res_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('<h1>Hello Customer!</h1>')
+    return render(request, 'hot_res_app/home.html')
