c099adc4094bf4f0cb511b5ce35fed1bdc595fe8
november-pain <max.chikita@gmail.com>
2020-11-08 02:49:12 +0200

    created models for database

--- hot_res_djng/hot_res_app/models.py
+++ hot_res_djng/hot_res_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField()
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE)
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+
+
+class Order(models.Model):
+    order = models.JSONField()
+    date_time = models.DateTimeField(default=timezone.now)
+
+
+class Places(models.Model):
+    name = models.CharField(max_length=100)
+    qr_code = models.ImageField()
