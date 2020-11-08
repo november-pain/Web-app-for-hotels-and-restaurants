6cb918448484e4d1a47ca4d3a6dcf97609582209
november-pain <max.chikita@gmail.com>
2020-11-08 05:39:14 +0200

    models registered in admin pannel

--- hot_res_djng/hot_res_app/admin.py
+++ hot_res_djng/hot_res_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Menu, Category, Order, Place
 
-# Register your models here.
+admin.site.register(Menu)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Place)
