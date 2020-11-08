cebc157123739d721648bc31c730c7eee24eb47c
november-pain <max.chikita@gmail.com>
2020-11-08 05:40:44 +0200

    rendering context

--- hot_res_djng/hot_res_app/views.py
+++ hot_res_djng/hot_res_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Menu, Category, Place, Order
 from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'hot_res_app/home.html')
+    context = {
+        'categories': Category.objects.all()
+    }
+    return render(request, 'hot_res_app/home.html', context)
