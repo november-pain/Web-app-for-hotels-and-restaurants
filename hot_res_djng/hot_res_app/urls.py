923ba9a631feebc2d35cca9d05ef301fbeaaba3c
november-pain <max.chikita@gmail.com>
2020-11-14 19:06:50 +0200

    added routes for different places and route for loading thing from db

--- hot_res_djng/hot_res_app/urls.py
+++ hot_res_djng/hot_res_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index-page')
-]
+urlpatterns = [path(str(i), views.index, name='index-page') for i in range(10)]
+urlpatterns.extend([
+    path('', views.index, name='index-page'),
+    path('menu/', views.load_from_db)
+])
+
