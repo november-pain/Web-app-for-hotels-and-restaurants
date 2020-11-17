01406e29b8a5c4ea1c2a2f964720c907e65fb14e
november-pain <max.chikita@gmail.com>
2020-11-17 18:31:54 +0200

    optimizing code

--- hot_res_djng/hot_res_app/src/components/item.js
+++ hot_res_djng/hot_res_app/src/components/item.js
@@ -6,21 +6,16 @@ export default class Item extends React.Component {
         super(props);
     
         this.state = {
-          item: {
-              name: props.name,
-              description: props.description,
-              price: props.price,
-              category: props.category
-          },
+          visible: props.visible
         };
     }
     render() {
         return (
-            <div>
-                <h2>{this.state.item.name}</h2>
-                <h3>price: {this.state.item.price}</h3>
-                <h4>category: {this.state.item.category}</h4>
-                <h5>description: {this.state.item.description}</h5>
+            <div className={this.state.visible ? "visible" : "hidden"}>
+                <h2>{this.props.name}</h2>
+                <h3>price: {this.props.price}</h3>
+                <h4>category: {this.props.category}</h4>
+                <h5>description: {this.props.description}</h5>
             </div>
         )
     }
