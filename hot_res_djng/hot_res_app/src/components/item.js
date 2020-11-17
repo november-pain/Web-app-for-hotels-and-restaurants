369adc5ec5b5afee2580343a50ebdcbffd4b07be
november-pain <max.chikita@gmail.com>
2020-11-17 21:24:40 +0200

    added conditional rendering of menu item

--- hot_res_djng/hot_res_app/src/components/item.js
+++ hot_res_djng/hot_res_app/src/components/item.js
@@ -4,14 +4,16 @@ import React from 'react';
 export default class Item extends React.Component {
     constructor(props) {
         super(props);
-    
-        this.state = {
-          visible: props.visible
-        };
     }
     render() {
+        if (this.props.chosenCategory) {
+                if (this.props.category !== this.props.chosenCategory) {
+                    return (<div/>)
+                };
+
+            };
         return (
-            <div className={this.state.visible ? "visible" : "hidden"}>
+            <div>
                 <h2>{this.props.name}</h2>
                 <h3>price: {this.props.price}</h3>
                 <h4>category: {this.props.category}</h4>
