7f01e22d48db7dc40e6da6ad69f0815ad0e82a32
november-pain <max.chikita@gmail.com>
2020-11-17 03:37:30 +0200

    added loop that renders each menu item

--- hot_res_djng/hot_res_app/src/components/menu.js
+++ hot_res_djng/hot_res_app/src/components/menu.js
@@ -1,30 +1,53 @@
 import React from 'react';
+import Item from './item.js';
 
 
 export default class Menu extends React.Component {
-    state = {
-        loading: true,
-        item: null
-    };
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            loading: true,
+            menu: null
+        };  
 
-    async componentDidMount(){
+        this.renderItem = this.renderItem.bind(this);
+
+    }
+    async componentDidMount() {
         const url = "http://localhost:8000/menu/";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ item: data, loading: false});
-
+        this.setState({ menu: JSON.parse(data), loading: false});
     }
+
+    renderItem() {
+        const itemList = [];
+        for(let i = 0; i < this.state.menu.length; i++) {
+            let name = this.state.menu[i]["fields"]["name"];
+            let price = this.state.menu[i]["fields"]["price"];
+            let category = this.state.menu[i]["fields"]["category"];
+            let description = this.state.menu[i]["fields"]["description"];
+            let key = this.state.menu[i]["pk"];
+            itemList.push(<Item name={name} price={price} category={category} description={description} key={key} />);
+            
+        }
+        return itemList;
+    }
+
     render() {
         if (this.state.loading) {
             return <div>loading..</div>;
         }
-        if (!this.state.item) {
+        if (!this.state.menu) {
             return <div>didn`t get item</div>
         }
-        return (
-            <div>
-                <p>{this.state.item}</p>
-            </div>
-        );
+        if (this.state.menu){
+            return (
+                <div>
+                    {this.renderItem()}
+                </div>
+            );
+        }
     }
 }
