dfaa5a0d642398dc523d340ea08d8aa305cc2110
november-pain <max.chikita@gmail.com>
2020-11-24 14:25:58 +0200

    part of code moved to allCategories.js

--- hot_res_djng/hot_res_app/src/components/menu.js
+++ hot_res_djng/hot_res_app/src/components/menu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Category from './category.js';
 import Item from './item.js';
 
 
@@ -10,50 +9,17 @@ export default class Menu extends React.Component {
         this.state = {
             loading: true,
             menu: null,
-            categories: null,
             chosenCategory: null
         };  
 
     }
 
-    async fetchMenu() {
+    async componentDidMount() {
         const url_menu = window.location.href + 'db/menu';
         const menu_response = await fetch(url_menu);
         const menu_data = await menu_response.json();
 
-        this.setState({ menu: JSON.parse(menu_data)});
-    }
-
-    async fetchCategories() {
-        const url_categories = window.location.href + 'db/categories';
-        const categories_response = await fetch(url_categories);
-        const categories_data = await categories_response.json();
-
-        this.setState({ categories: JSON.parse(categories_data), loading: false});
-    }
-
-    async componentDidMount() {
-        this.fetchMenu()
-        this.fetchCategories()
-    }
-    
-    chooseCategory = (id) => {
-        this.setState({
-            chosenCategory: id
-        })
-    }
-
-    renderCategory = () => {
-        const categoryList = [];
-        for(let i = 0; i < this.state.categories.length; i++) {
-            let name = this.state.categories[i]["fields"]["name"];
-            let id = this.state.categories[i]["pk"];
-
-            categoryList.push(
-                <Category name={name} chooseCategory={this.chooseCategory} id={id} key={id} />
-            );
-        }
-        return categoryList;
+        this.setState({ menu: JSON.parse(menu_data), loading: false});
     }
 
     renderItem = () => {
@@ -65,7 +31,7 @@ export default class Menu extends React.Component {
             let description = this.state.menu[i]["fields"]["description"];
             let key = this.state.menu[i]["pk"];
 
-            itemList.push(<Item name={name} price={price} category={category} description={description} chosenCategory={this.state.chosenCategory} key={key} />);
+            itemList.push(<Item name={name} price={price} category={category} description={description} chosenCategory={this.props.chosenCategory} key={key} />);
             
         }
         return itemList;
@@ -80,19 +46,9 @@ export default class Menu extends React.Component {
             return <div>didn`t get item</div>
         }
         if (!this.state.loading){
-            return (
-                <div>                   
-                    <div className="categories">
-                        <button onClick={() => {
-                            this.chooseCategory(null)
-                        }} className="category-button">
-                            All
-                        </button>
-                        {this.renderCategory()}
-                    </div>
-                    <div className="menu">
-                        {this.renderItem()}
-                    </div>
+            return (                
+                <div className="menu">
+                    {this.renderItem()}
                 </div>
             );
         }
