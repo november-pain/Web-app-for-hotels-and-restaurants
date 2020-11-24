64310530066e3d4905e0cb3e8b2d93615500b7c1
november-pain <max.chikita@gmail.com>
2020-11-25 01:33:43 +0200

    collapsible button added

--- hot_res_djng/hot_res_app/src/components/cart.js
+++ hot_res_djng/hot_res_app/src/components/cart.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Collapse } from 'antd';
+
+
+const { Panel } = Collapse;
 
 
 export default class Cart extends React.Component {
@@ -6,10 +10,18 @@ export default class Cart extends React.Component {
         super(props);
 
     }
+
+
     render() {
         return (
             <div className="cart">
-                <button className="order-button">Cart</button>
+                <Collapse className="order-button" defaultActiveKey={['1']} >
+                    <Panel className="order-button" showArrow={false} header="Your cart" key="1">
+                        <div className="order">
+                            <p>you haven't ordered anything</p>
+                        </div>
+                    </Panel>
+                </Collapse>    
             </div>
         )
     }
