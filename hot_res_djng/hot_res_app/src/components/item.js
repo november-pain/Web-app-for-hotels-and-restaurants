a3bcd0c504979dfed9084ccde6b651562dd32748
eyehol3 <eyeh0l3@gmail.com>
2020-11-26 23:53:26 +0200

    cart logic

--- hot_res_djng/hot_res_app/src/components/item.js
+++ hot_res_djng/hot_res_app/src/components/item.js
@@ -1,28 +1,44 @@
-import React from 'react';
-
+import React from "react";
 
 export default class Item extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        if (this.props.chosenCategory) {
-                if (this.props.category !== this.props.chosenCategory) {
-                    return (<div/>)
-                };
+  constructor(props) {
+    super(props);
+  }
 
-            };
-        return (
-            <div className="menu-item">
-                <h2>{this.props.name}</h2>
-                <h3>price: {this.props.price}</h3>
-                <h4>category: {this.props.category}</h4>
-                <h5>description: {this.props.description}</h5>
-                <button className="add-to-cart" onClick={() => {
-                    this.props.setOrder(this.props.name, this.props.id)}}>
-                        +
-                        </button>
-            </div>
-        )
+  addItem = () => {
+    if (this.props.order[this.props.id] != null) {
+      this.props.setOrder({
+        [this.props.id]: {
+          number: this.props.order[this.props.id].number + 1,
+          name: this.props.name,
+        },
+      });
+    } else {
+      this.props.setOrder({
+        [this.props.id]: {
+          number: 1,
+          name: this.props.name,
+        },
+      });
     }
+  };
+
+  render() {
+    if (this.props.chosenCategory) {
+      if (this.props.category !== this.props.chosenCategory) {
+        return <div />;
+      }
+    }
+    return (
+      <div className="menu-item">
+        <h2>{this.props.name}</h2>
+        <h3>price: {this.props.price}</h3>
+        <h4>category: {this.props.category}</h4>
+        <h5>description: {this.props.description}</h5>
+        <button className="add-to-cart" onClick={this.addItem}>
+          +
+        </button>
+      </div>
+    );
+  }
 }
