99571c55046fb394b0fbc907679abb4574ffa328
november-pain <max.chikita@gmail.com>
2020-12-02 23:58:39 +0200

    render post request in hot_res_app

--- hot_res_djng/hot_res_app/urls.py
+++ hot_res_djng/hot_res_app/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('<slug:place>', views.index),
     path('', views.index, name='index-page'),
+
+    path('<slug:place>/post/order', views.order_post),
+    path('post/order', views.order_post),
+
     path('db/<slug:load>/', views.load_from_db)
     # re_path(r'^db/(?P<load>[\w.@+-]+)/$', views.load_from_db),
     
