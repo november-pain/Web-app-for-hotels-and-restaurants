10946e4135321fcf12607ca354d77b397f63c62d
eyehol3 <eyeh0l3@gmail.com>
2020-12-05 14:36:40 +0200

    fixed remove_item

--- hot_res_djng/hot_res_app/src/components/app.js
+++ hot_res_djng/hot_res_app/src/components/app.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Menu from "./menu.js";
 import AllCategories from "./allCategories.js";
 import Cart from "./cart.js";
 import "../styles/app.scss";
 import "antd/dist/antd.css";
+import { OrderContext } from "./orderContext.js";
 
 const App = () => {
 	const debugOrder = true;
 	const readOrderFromStorage = () => {
 		if (JSON.parse(localStorage.getItem("order"))) {
-            console.log("order is not null");
 			return JSON.parse(localStorage.getItem("order"));
 		} else {
-            console.log("order is null")
 			return {};
 		}
 	};
@@ -21,6 +20,8 @@ const App = () => {
 
 	const [chosenCategory, setCategory] = useState(null);
 
+	const renders = useRef(0);
+
 	const appendOrder = (appendOrder) => {
 		setOrder((currentOrder) => ({
 			...currentOrder,
@@ -28,22 +29,35 @@ const App = () => {
 		}));
 	};
 
+	const removeItem = (id) => {
+		setOrder((o) => {
+			let tmporder = { ...o };
+			delete tmporder[id];
+			return tmporder;
+		});
+	};
+
 	useEffect(() => {
-		if (debugOrder) {
-			console.log("order: ", order);
-		}
 		localStorage.setItem("order", JSON.stringify(order));
 	}, [order]);
 
 	return (
 		<div>
-			<AllCategories setCategory={setCategory} />
-			<Cart order={order} appendOrder={appendOrder} setOrder={setOrder} />
-			<Menu
-				chosenCategory={chosenCategory}
-				appendOrder={appendOrder}
-				order={order}
-			/>
+			<OrderContext.Provider value={{ order, setOrder }}>
+				<AllCategories setCategory={setCategory} />
+				{debugOrder ? (
+					<div>
+						<pre>{JSON.stringify(order)}</pre>
+						<p>renders: {renders.current++}</p>
+					</div>
+				) : null}
+				<Cart appendOrder={appendOrder} removeItem={removeItem} />
+				<Menu
+					chosenCategory={chosenCategory}
+					appendOrder={appendOrder}
+					order={order}
+				/>
+			</OrderContext.Provider>
 		</div>
 	);
 };
