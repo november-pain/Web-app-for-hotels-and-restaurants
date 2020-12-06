dce8b905b4b8b8069e8c2250e31af578b5a57900
BurlyLlama28 <kiltik12@gmail.com>
2020-12-07 01:43:09 +0200

    added clock

---
+++ hot_res_djng/waiter/src/components/clock.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString(),
+    });
+  }
+  render() {
+    return <p className="App-clock">{this.state.time}</p>;
+  }
+}
