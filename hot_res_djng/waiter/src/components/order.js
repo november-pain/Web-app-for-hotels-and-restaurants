512d8fec3ba7f2959243c193a896a10352b2eeb8
BurlyLlama28 <kiltik12@gmail.com>
2020-12-06 21:52:45 +0200

    see list of orders

---
+++ hot_res_djng/waiter/src/components/order.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Orders = [
+  { 1: { name: "Fried eggs", number: 3 } },
+  { 1: { name: "Fried chef eggs", number: 4 } },
+  { 1: { name: "Fried eggs", number: 5 } },
+  { 1: { name: "Fried Naruto's eggs", number: 6 } },
+];
+
+export default class List_of_Orders extends React.Component {
+  render() {
+    return Orders.map((o) => JSON.stringify(o));
+  }
+}
