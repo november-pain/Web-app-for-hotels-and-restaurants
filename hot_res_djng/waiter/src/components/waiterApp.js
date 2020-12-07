7d5a9c8a0550a947e1c19598cc79de87adc878cc
BurlyLlama28 <kiltik12@gmail.com>
2020-12-07 18:06:20 +0200

    clock update + scss

--- hot_res_djng/waiter/src/components/waiterApp.js
+++ hot_res_djng/waiter/src/components/waiterApp.js
@@ -1,16 +1,17 @@
 import React from "react";
 import List_of_Orders from "./order";
 import Clock from "./clock";
+import "../styles/waiter_app.scss";
 
 export default class App extends React.Component {
   render() {
     return (
-      <h4>
-        <div>
-          <Clock></Clock>
+      <div>
+        <Clock></Clock>
+        <div kry={1}>
           <List_of_Orders></List_of_Orders>
         </div>
-      </h4>
+      </div>
     );
   }
 }
