dfb5fb9d96888c3068313f9f98db8944357c4831
november-pain <max.chikita@gmail.com>
2020-12-08 20:27:14 +0200

    bug fixed

--- hot_res_djng/waiter/src/components/listOfOrders.js
+++ hot_res_djng/waiter/src/components/listOfOrders.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Order from "./order.js";
 
-// const OOOrders = [
-//   { 1: { name: "Fried eggs", number: 3 } },
-//   { 1: { name: "Fried chef eggs", number: 4 } },
-//   { 1: { name: "Fried eggs", number: 5 } },
-//   { 1: { name: "Fried Naruto's eggs", number: 6 } },
-// ];
 
 export default class ListOfOrders extends React.Component {
   constructor(props) {
@@ -27,53 +21,32 @@ export default class ListOfOrders extends React.Component {
     this.setState({
       orders: orders_json,
       loading: false,
-    });
-    // console.log(this.state.orders);
+    }, () => {console.log(typeof(this.state.orders[0]["pk"]))});
   }
 
   renderOrders = () => {
-    console.log(this.state.orders)
-    const orders = this.state.orders
-    console.log(orders) 
-
-    // ERROR comes here, you cannot do anything with an array
-    // try console.log(orders[0]) or try console.log(orders.length) or any operation with it
-
-    // const ordersList = orders.map(
-    //     (order) => 
-    //     <Order
-    //       order={order["fields"]["order"]}
-    //       date_time={order["fields"]["date_time"]}
-    //       key={order["pk"]}
-    //      />);
-    const ordersList = []
-
-
-    // for (let i = 0; i < this.state.orders.length; i++) {
-    //   console.log(i)
-    //   let id = this.state.orders[i]["pk"];
-    //   let date_time = this.state.orders[i]["fields"]["date_time"];
-    //   let order = this.state.orders[i]["fields"]["order"];
-
-    //   ordersList.push(
-    //     <Order order={order} date_time={date_time} id={id} key={id} /> // key={id}
-    //   );
-    // }
+    const ordersList = this.state.orders.map(
+        (ord) => 
+          <Order
+            order={ord["fields"]["order"]}
+            date_time={ord["fields"]["date_time"]}
+            key={ord["pk"]}
+          />
+        );
     return ordersList;
   };
 
   render() {
-    // return OOOrders.map((o, index) => <p key={index}>{JSON.stringify(o)}</p>);
-    console.log(this.state.loading)
-    // console.log(this.state.orders)
+    if (this.state.loading) {
+      return <div>loading...</div>
+    }
+    else {
+      return (
+          <div className="orders">
+            {this.renderOrders()}
+          </div>
+      )
+    }
 
-
-    // if (!this.state.loading) {
-    return (
-        <div className="orders">
-          {this.renderOrders()}
-        </div>
-    )
-    // }
   }
 }
--- hot_res_djng/waiter/src/components/order.js
+++ hot_res_djng/waiter/src/components/order.js
@@ -8,10 +8,8 @@ export default class Order extends React.Component {
   render() {
     return (
       <div className="listOfOrders-order">
-        {/* <h2>{this.props.name}</h2>
-        <h3>price: {this.props.number}</h3> */}
-        <h4>date_time: {this.props.date_time}</h4>
-        <h5>order: {this.props.order}</h5>
+        <h4>date time: {this.props.date_time}</h4>
+        <h5>order: {JSON.stringify(this.props.order)}</h5>
       </div>
     )
   }
