3d40500c64c1d82fc465403fd118a8fd6d78b080
BurlyLlama28 <kiltik12@gmail.com>
2020-12-09 18:42:44 +0200

    formOrders

--- hot_res_djng/waiter/src/components/order.js
+++ hot_res_djng/waiter/src/components/order.js
@@ -1,16 +1,41 @@
 import React from "react";
 
+const formOrder = (smth) => {
+  let order = [];
+  for (var i in smth) {
+    order.push(
+      JSON.stringify(i),
+      " name:",
+      JSON.stringify(smth[i].name),
+      " number:",
+      JSON.stringify(smth[i].number),
+      " "
+    );
+  }
+  return order;
+};
+
 export default class Order extends React.Component {
   constructor(props) {
     super(props);
+    // this.state.order = null;
+    this.state = {
+      order: null,
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ order: formOrder(this.props.order), loading: false });
   }
 
   render() {
-    return (
-      <div className="listOfOrders-order">
-        <h4>date time: {this.props.date_time}</h4>
-        <h5>order: {JSON.stringify(this.props.order)}</h5>
-      </div>
-    )
+    if (this.state.loading) {
+      return null;
+    } else {
+      return (
+        <div className="order">order: {this.state.order.map((ord) => ord)}</div>
+      );
+    }
   }
 }
