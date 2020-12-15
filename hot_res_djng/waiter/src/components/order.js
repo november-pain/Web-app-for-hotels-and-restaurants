37f58ad1282d690792abb916e3136c39b2dab24d
BurlyLlama28 <kiltik12@gmail.com>
2020-12-15 19:39:08 +0200

    waiter-app css update

--- hot_res_djng/waiter/src/components/order.js
+++ hot_res_djng/waiter/src/components/order.js
@@ -1,50 +1,55 @@
 import React, { useState } from "react";
-import { format } from 'date-fns'
-import { Button } from 'antd'
+import { format } from "date-fns";
+import { Button } from "antd";
 
 export default (props) => {
-  const { order, date_time, id} = props
-  
-  const [loading, setLoading] = useState(false)
-  
+  const { order, date_time, id } = props;
+
+  const [loading, setLoading] = useState(false);
+
   const renderOrder = () => {
-    const itemList = []
-    const orderArr = Object.entries(order)
+    const itemList = [];
+    const orderArr = Object.entries(order);
     for (let i = 0; i < orderArr.length; i++) {
       itemList.push(
-        <li key={i}> {orderArr[i][1].name} x {orderArr[i][1].number}</li>
-      )
+        <li className="item" key={i}>
+          {" "}
+          {orderArr[i][1].name} x {orderArr[i][1].number}
+        </li>
+      );
     }
-    return itemList
-  }
-  
-  const formatDate = () => {
-    let dateString = Date.parse(date_time)
-    let date = new Date(dateString)
-    return format(date, 'MM/dd/yyyy HH:mm')
-  }
+    return itemList;
+  };
 
-  const completeOrder = async() => {
+  const formatDate = () => {
+    let dateString = Date.parse(date_time);
+    let date = new Date(dateString);
+    return format(date, "MM/dd/yyyy HH:mm");
+  };
+
+  const completeOrder = async () => {
     fetch(window.location.origin + "/post/orderdone/", {
-			method: "POST",
-			headers: { "Content-Type": "application/text" },
-			body: JSON.stringify(id),
-		});
-  }
+      method: "POST",
+      headers: { "Content-Type": "application/text" },
+      body: JSON.stringify(id),
+    });
+  };
 
   return (
     <div className="order">
-      <h3>order:</h3>
-      <ol>
-        {renderOrder()}
-      </ol>
-      <p>{formatDate()}</p>
-      <Button type="primary" loading={loading} onClick={() => {
-        setLoading(true)
-        completeOrder().then(setLoading(false))
-      }}>
+      <h3 className="order_name">order:</h3>
+      <ol className="item_list">{renderOrder()}</ol>
+      <p className="date">{formatDate()}</p>
+      <Button
+        type="primary"
+        loading={loading}
+        onClick={() => {
+          setLoading(true);
+          completeOrder().then(setLoading(false));
+        }}
+      >
         Done
       </Button>
     </div>
   );
-}
+};
