4dbd3f71940dd981ca5b379067eb5d3e87bbcf10
BurlyLlama28 <kiltik12@gmail.com>
2020-12-16 17:59:39 +0200

    reversed ordersList sort try

--- hot_res_djng/waiter/src/components/listOfOrders.js
+++ hot_res_djng/waiter/src/components/listOfOrders.js
@@ -1,55 +1,70 @@
 import React, { useState, useEffect } from "react";
 import Order from "./order.js";
+import { Button } from "antd";
 
 const ListOfOrders = () => {
-	const [{ orders, loading }, setState] = useState({
-		orders: null,
-		loading: true,
-	});
+  const [{ orders, loading }, setState] = useState({
+    orders: null,
+    loading: true,
+  });
 
-	const onMount = async () => {
-		loadData();
-		setInterval(async () => {
-			loadData();
-		}, 15000);
-	};
+  const onMount = async () => {
+    loadData();
+    setInterval(async () => {
+      loadData();
+    }, 15000);
+  };
 
-	useEffect(() => {
-		onMount();
-	}, []);
+  useEffect(() => {
+    onMount();
+  }, []);
 
-	const loadData = async () => {
-		const url_orders = window.location.origin + "/db/orders";
-		const orders_response = await fetch(url_orders);
-		const orders_data = await orders_response.json();
-		const orders_json = JSON.parse(orders_data);
+  const loadData = async () => {
+    const url_orders = window.location.origin + "/db/orders";
+    const orders_response = await fetch(url_orders);
+    const orders_data = await orders_response.json();
+    const orders_json = JSON.parse(orders_data);
 
-		setState((state) => ({
-			...state,
-			orders: orders_json,
-			loading: false,
-		}));
-	};
+    setState((state) => ({
+      ...state,
+      orders: orders_json,
+      loading: false,
+    }));
+  };
 
-	const renderOrders = () => {
-		const ordersList = orders
-			.reverse()
-			.map((ord) => (
-				<Order
-					order={ord["fields"]["order"]}
-					date_time={ord["fields"]["date_time"]}
-					key={ord["pk"]}
-					id={ord["pk"]}
-				/>
-			));
-		return ordersList;
-	};
+  var ordersList = [];
 
-	if (loading) {
-		return <div>loading...</div>;
-	} else {
-		return <div className="listOfOrders">{renderOrders()}</div>;
-	}
+  const renderOrders = () => {
+    ordersList = orders.map((ord) => (
+      <Order
+        order={ord["fields"]["order"]}
+        date_time={ord["fields"]["date_time"]}
+        key={ord["pk"]}
+        id={ord["pk"]}
+      />
+    ));
+    return ordersList;
+  };
+
+  if (loading) {
+    return <div>loading...</div>;
+  } else {
+    renderOrders();
+    return (
+      <div className="listOfOrders">
+        <Button
+          type="primary"
+          onClick={() => {
+            ordersList = ordersList.reverse();
+            console.log(ordersList);
+          }}
+        >
+          Sort by date
+        </Button>
+        {ordersList}
+      </div>
+    );
+  }
 };
 
 export default ListOfOrders;
