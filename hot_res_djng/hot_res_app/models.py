db8865e9ca29045c66b480caefcf023eb14fffc7
november-pain <max.chikita@gmail.com>
2021-02-08 20:36:16 +0200

    qr code generator in model place

--- hot_res_djng/hot_res_app/models.py
+++ hot_res_djng/hot_res_app/models.py
@@ -1,25 +1,32 @@
 from django.db import models
 from django.utils import timezone
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
+from hot_res_djng.settings import IP
 
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="hot_res_app/static/hot_res_app/images/menu_items")
+    image = models.ImageField(
+        upload_to="hot_res_app/static/hot_res_app/images/menu_items")
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="hot_res_app/static/hot_res_app/images/categories")
+    image = models.ImageField(
+        upload_to="hot_res_app/static/hot_res_app/images/categories")
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Order(models.Model):
@@ -35,7 +42,22 @@ class Completed_Order(models.Model):
 
 class Place(models.Model):
     name = models.CharField(max_length=100)
-    qr_code = models.ImageField()
+    url = models.URLField(default="http://{}:8000/".format(IP))
+    qr_code = models.ImageField(
+        upload_to="hot_res_app/static/hot_res_app/images/qr_codes", blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        qr_img = qrcode.make(self.url)
+        canvas = Image.new('RGB', (qr_img.pixel_size,
+                                   qr_img.pixel_size), 'white')
+        canvas.paste(qr_img)
+        file_name = 'qr_code_{}.png'.format(self.name)
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(file_name, File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
+
