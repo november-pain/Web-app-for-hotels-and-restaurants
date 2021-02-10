46bc206d5db8c2fc9ac83b4f9ed36d5138c6aca8
november-pain <max.chikita@gmail.com>
2021-02-10 19:27:08 +0200

    login and logout views added

--- hot_res_djng/waiter/views.py
+++ hot_res_djng/waiter/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+@login_required(login_url='login')
 def waiter_page(request):
     return render(request, 'waiter/waiter.html')
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('waiter')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('waiter')
+            else:
+                messages.error(request, 'Username or password is invalid')
+
+        return render(request, 'waiter/login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
