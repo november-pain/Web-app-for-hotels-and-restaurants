0f6a3f74bf82ae5cc8c523271854c187a848aa59
november-pain <max.chikita@gmail.com>
2021-02-15 04:01:15 +0200

    bug with paths fixed and refactoring

--- hot_res_djng/hot_res_app/src/components/cartItem.js
+++ hot_res_djng/hot_res_app/src/components/cartItem.js
@@ -5,7 +5,7 @@ import { MenuContext, OrderContext } from "./сontext";
 export default (props) => {
 	const { order, appendOrder, removeItem } = useContext(OrderContext);
 	const { id, picture } = props;
-    const { menu } = useContext(MenuContext)
+	const { menu } = useContext(MenuContext);
 
 	const increment = () => {
 		appendOrder({
@@ -28,17 +28,24 @@ export default (props) => {
 			removeItem(id);
 		}
 	};
-    const getPrice = (id) => menu.find(v=>v.pk==id).fields.price;
-	if (order[id].number > 0 && menu!=null) {
+	const getPrice = (id) => menu.find((v) => v.pk == id).fields.price;
+	if (order[id].number > 0 && menu != null) {
 		return (
 			<div id="cart-item">
 				<div className="image">
-					<img src={"../static/" + picture.split("static")[1]} alt="" />
+					<img
+						src={"../../static/" + picture.split("static")[1]}
+						alt=""
+					/>
 				</div>
 				<p className="name">{order[id].name}</p>
 				<p className="price">₴{getPrice(id)}</p>
 				<span>
-					<Counter increment={increment} decrement={decrement} count={order[id].number}/>
+					<Counter
+						increment={increment}
+						decrement={decrement}
+						count={order[id].number}
+					/>
 				</span>
 			</div>
 		);
