d17aba6c1cb38b5e3badb9218e93803b96c2d454
november-pain <max.chikita@gmail.com>
2021-02-16 18:06:29 +0200

    compression module added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,33 @@
+// const CompressionPlugin = require("compression-webpack-plugin");
+// const zlib = require("zlib");
+// const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
 	watch: true,
+	// optimization: {
+	// 	minimize: true,
+	//   },
+	// plugins: [
+	// 	new CompressionPlugin({
+	// 		filename: "[path][base].gz",
+	// 		algorithm: "gzip",
+	// 		test: /\.js$|\.css$|\.html$/,
+	// 		threshold: 10240,
+	// 		minRatio: 0.8,
+	// 	}),
+	// 	new CompressionPlugin({
+	// 		filename: "[path][base].br",
+	// 		algorithm: "brotliCompress",
+	// 		test: /\.(js|css|html|svg)$/,
+	// 		compressionOptions: {
+	// 			params: {
+	// 				[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+	// 			},
+	// 		},
+	// 		threshold: 10240,
+	// 		minRatio: 0.8,
+	// 	}),
+	// ],
 	module: {
 		rules: [
 			{
