aa0fc55867f31bf96ccb5c74d32604cea4ed60f5
november-pain <max.chikita@gmail.com>
2021-02-16 12:46:56 +0200

    basic urls and views for call waiter request

--- hot_res_djng/hot_res_app/urls.py
+++ hot_res_djng/hot_res_app/urls.py
@@ -4,9 +4,7 @@ from . import views
 urlpatterns = [
     path('<slug:place>/', views.index),
     path('', views.index, name='index-page'),
-
     path('<slug:place>/post/order', views.order_post),
-    path('post/order', views.order_post),
-
     path('db/<slug:load>/', views.load_from_db),
+    path('<slug:place>/post/call_waiter>', views.call_waiter)
 ]
