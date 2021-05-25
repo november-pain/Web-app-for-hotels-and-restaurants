493d79ee135a0db8c225e07e914662e05980b7a2
eyehol3 <eyeh0l3@gmail.com>
2021-05-26 00:23:46 +0300

    jest setup

---
+++ hot_res_djng/hot_res_app/src/components/tests/app.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import app from '../app';
+
+// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
+// unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+
+it('Empty test runs', () => {
+  expect(true).toBeTruthy();
+});
